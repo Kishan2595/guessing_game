9bcbd5bd2c45fef1cd09e0352eecfd324c0c9ccf
Kishan2595 <kishan95.kumar@gmail.com>
2020-07-29 07:34:32 +0530

    Generating a Random number & compairing it to input guess

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use std::io;
+use std::io;                              // library-ability to accept user imput
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     
     println!("Guess a number");
-
+    let secret_number = rand::thread_rng().gen_range(1, 101);               //Generating a random number
+    println!("The secret number is: {}", secret_number); 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess = String::new();        // created a mutable var i.e bound to a new empty instance of a str.
 
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+    io::stdin()                           // stdin of io module is a type that represemts a handles to std input.  
+    .read_line(&mut guess)                // read_line takes whatever the user input     
+    .expect("Failed to read line");       // .expect is to hsndle err
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You Win!"),
+    }
 }
